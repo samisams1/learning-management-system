6897b35b365078f54aef58fd1e6a0191a8076ca6
samisams1 <forsamisams@gmail.com>
2023-06-21 07:43:59 +0300

    edit header

--- front-end/src/components/common/header/Header.jsx
+++ front-end/src/components/common/header/Header.jsx
@@ -1,47 +1,48 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './header.css'
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import Head from "./head"
+import "./header.css"
 
 const Header = () => {
-    const [click, setClick] = useState(false)
-  
+  const [click, setClick] = useState(false)
+
   return (
-  <>
-  <header>
-    <nav className='flexSB'>
-       <ul className='samisam'>
-          <li>
-             <Link to ='/'>Hom</Link>
-          </li>
-          <li>
-            <Link to ='courses'>All Courses</Link>
-          </li>
-          <li>
-            <Link to ='courses'>About</Link>
-          </li>
-          <li>
-            <Link to ='courses'>Team</Link>
-          </li>
-          <li>
-            <Link to ='courses'>Pricing</Link>
-          </li>
-          <li>
-            <Link to ='/journal'>Journal</Link>
-          </li>
-        <li>
-            <Link to ='contact'>Contact</Link>
-        </li>
-       </ul>
-       <div className='start'>
-        <div className='button'>GET CERTIFICATE
-        </div>
-    <button className='toggle' onClick={()=>setClick(!click)}>
-    {click ? <i className='fa fa-times'> </i> : <i className='fa fa-bars'></i>}
-    </button>
-       </div>
-    </nav>
-  </header>
-  </>
+    <>
+    <Head/>
+      <header>
+        <nav className='flexSB'>
+          <ul className={click ? "mobile-nav" : "flexSB "} onClick={() => setClick(false)}>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/courses'>All Courses</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/team'>Team</Link>
+            </li>
+            <li>
+              <Link to='/pricing'>Pricing</Link>
+            </li>
+            <li>
+              <Link to='/journal'>Journal</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className='start'>
+            <div className='button'>GET CERTIFICATE</div>
+          </div>
+          <button className='toggle' onClick={() => setClick(!click)}>
+            {click ? <i className='fa fa-times'> </i> : <i className='fa fa-bars'></i>}
+          </button>
+        </nav>
+      </header>
+    </>
   )
 }
 
--- front-end/src/components/common/header/head.jsx
+++ front-end/src/components/common/header/head.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react"
 
-const Head=()=> {
+const Head = () => {
   return (
-    <div>
-      Header
-    </div>
+    <>
+      <section className='head'>
+        <div className='container flexSB'>
+          <div className='logo'>
+            <h1>ACADEMIA</h1>
+            <span>ONLINE EDUCATION & LEARNING</span>
+          </div>
+
+          <div className='social'>
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-instagram icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-youtube icon'></i>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
-export default Head;
+export default Head
