c9b722f9e936095932ffc5acdd77308d65f4ac10
samisams1 <forsamisams@gmail.com>
2023-06-21 10:22:31 +0300

    create testimonal

--- front-end/src/components/home/Home.jsx
+++ front-end/src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AboutCard from '../about/AboutCard'
 import HAbout from './HAbout'
 import Hero from './hero/Hero'
+import Testimonal from './testimonal/Testimonal'
 
 function Home() {
   return (
@@ -9,7 +10,7 @@ function Home() {
     <Hero />
     <AboutCard/>
      <HAbout/>
-     
+     <Testimonal/>
     </>
   )
 }
