ec3a43dc06c83ccb7a8a6dba97df8fb0a5338c80
samisams1 <forsamisams@gmail.com>
2023-06-21 09:35:59 +0300

    add favicon

---
+++ front-end/src/components/about/AboutCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AboutCard = () => {
+  return (
+    <>
+     <div  className='aboutHome'>
+     <div className='container flexSB'>
+      <section>
+      <div className='left row'>
+            <img src='./images/about.webp' alt='' />
+          </div>
+      </section>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default AboutCard
