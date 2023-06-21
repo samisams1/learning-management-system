8961b85e1722a57d34d979508cb4a093dc64a67b
samisams1 <forsamisams@gmail.com>
2023-06-21 08:41:01 +0300

    add hero to image

--- front-end/src/components/home/hero/Hero.jsx
+++ front-end/src/components/home/hero/Hero.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-
+import Heading from '../../common/heading/Heading'
+import './hero.css'
 const Hero=()=> {
   return (
     <>
     <section className='hero'>
         <div className='container'>
            <div className='row'>
-                
+           <Heading subtitle='WELCOME TO ADDIS ABABA' title='Best Online Education Expertise' />
+           <p>this platform is best online learning managment system </p>
+           <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
            </div>
         </div>
     </section>
+    <div className='margin'></div>
     </>
   )
 }
